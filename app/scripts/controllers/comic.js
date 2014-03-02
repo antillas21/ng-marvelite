f3518afb8b96539fb74309856b955ce73584d309
Antonio Antillon <antillas21@gmail.com>
2014-03-02 08:07:00 -0800

    Adds connections to /series from other pages.

--- app/scripts/controllers/comic.js
+++ app/scripts/controllers/comic.js
@@ -2,8 +2,10 @@
 
 angular.module('ngMarveliteApp')
   .controller('ComicCtrl',
-    ['$scope', '$resource', '$routeParams', 'Comic',
-    function ($scope, $resource, $routeParams, Comic) {
+    ['$scope', '$resource', '$routeParams', 'Comic', 'urlParser',
+    function ($scope, $resource, $routeParams, Comic, urlParser) {
+      $scope.parser = urlParser;
+
       Comic.fetch($routeParams.id).then(function(data) {
         $scope.comic = data.results[0];
         $scope.loadComplete = true;
